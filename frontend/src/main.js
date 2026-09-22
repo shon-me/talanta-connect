78eff20433f172dce1cfe4facdf0c89e8e5e6e6c
shon-me <ngetichgershon@gmail.com>
2026-09-22 15:06:54 +0300

    vuetify init

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,9 +4,37 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+  },
+  theme: {
+    defaultTheme: 'light',
+    themes: {
+        light: {
+            colors: {
+                primary: "#F4511E",
+                secondary: "#FF3D00",
+            },
+        },
+    },
+  },
+})
+
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(vuetify)
 app.use(router)
 
 app.mount('#app')
